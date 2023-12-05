05a01ef4a646c854ac6bf5395fe1722d4748f3f9
Lylov <lylov@lanit.ru>
2023-12-05 17:56:58 +0300

    Задание 2 версия 1

--- ewm-main-service/src/main/java/ru/practicum/main/ExploreWithMe.java
+++ ewm-main-service/src/main/java/ru/practicum/main/ExploreWithMe.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"ru.practicum.stats"})
+@ComponentScan(basePackages = {"ru.practicum.stats", "ru.practicum.main"})
 public class ExploreWithMe {
     public static void main(String[] args) {
         SpringApplication.run(ExploreWithMe.class, args);
